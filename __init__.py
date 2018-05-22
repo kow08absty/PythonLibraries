835d78cdf0cb59cddeae4a93db9648f33ed6e5e0
Kohei OKUMURA <kow080816@gmail.com>
2018-05-23 06:44:14 +0900

    rename file, fix ConsoleColors, logging

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # from .<fileName> import <className>
-from .log import Log
-from .web_driver import FirefoxDriver, ChromeDriver
+from .log import Log, ConsoleColors
+from .webdriver import FirefoxDriver, ChromeDriver
 from .sqlite3 import SQLite3
